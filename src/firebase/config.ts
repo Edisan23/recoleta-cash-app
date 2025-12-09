9dbd5af193d2ac49eedbf215f12f8ea781412864
edward santiago riascos moreno <tjedisan@gmail.com>
2025-12-09 16:18:16 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "recoleta-cash-app",
   "appId": "1:402526181441:web:b6ea4e3ec970d9916e4ec0",
+  "storageBucket": "recoleta-cash-app.appspot.com",
   "apiKey": "AIzaSyAey-hchffDmO4rrIHkdWQSZkrN_KRliD0",
   "authDomain": "recoleta-cash-app.firebaseapp.com",
   "measurementId": "",
