d9375da7813a278ef1d9d8e7e7b0fd6f2f6974ce
edward santiago riascos moreno <tjedisan@gmail.com>
2025-12-15 08:57:27 +0000

    Try fixing this error: `Runtime ReferenceError: DialogTrigger is not def

--- src/components/OperatorDashboard.tsx
+++ src/components/OperatorDashboard.tsx
@@ -41,6 +41,7 @@ import {
     DialogFooter,
     DialogHeader,
     DialogTitle,
+    DialogTrigger,
 } from '@/components/ui/dialog';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
