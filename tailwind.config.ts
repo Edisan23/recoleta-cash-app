4961c47001b7fb59eed9ab415dd832eadd0259a2
edward santiago riascos moreno <tjedisan@gmail.com>
2026-01-14 12:28:25 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,8 @@ const config = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}'
-	],
+    './src/**/*.{ts,tsx}',
+  ],
   prefix: "",
   theme: {
     container: {
