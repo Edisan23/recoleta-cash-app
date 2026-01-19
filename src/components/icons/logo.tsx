14c7e7033d50dc038687d7cf81769fe213f8af1f
edward santiago riascos moreno <tjedisan@gmail.com>
2026-01-19 15:56:54 +0000

    necesito cambiar el icono de toda la aplicación, utilizar esta imagen co

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
+import { Truck } from 'lucide-react';
 
 export function LogoIcon({
   className,
   ...props
 }: React.ComponentProps<'svg'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <Truck
       className={cn('', className)}
       {...props}
-    >
-        <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
-    </svg>
+    />
   );
 }
