8dd2abf08a8115ad41294dae778ce8b7f162f602
edward santiago riascos moreno <tjedisan@gmail.com>
2026-01-20 03:28:13 +0000

    mostrar el logo del camion sin el fondo solo el camion amarillo

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 
-type LogoIconProps = React.HTMLAttributes<HTMLDivElement> & {
+type LogoIconProps = React.HTMLAttributes<SVGElement> & {
     className?: string;
 };
 
@@ -10,23 +10,17 @@ export function LogoIcon({
   ...props
 }: LogoIconProps) {
   return (
-    <div
-      style={{
-        maskImage: 'url(/favicon.svg)',
-        WebkitMaskImage: 'url(/favicon.svg)',
-        maskSize: 'contain',
-        WebkitMaskSize: 'contain',
-        maskRepeat: 'no-repeat',
-        WebkitMaskRepeat: 'no-repeat',
-        maskPosition: 'center',
-        WebkitMaskPosition: 'center',
-        backgroundColor: 'currentColor'
-      }}
-      className={cn(
-        "h-8 w-8 text-foreground", // Use text color to control the icon color
-        className
-      )}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 640 512"
+      className={cn("h-8 w-8", className)}
       {...props}
-    />
+    >
+      {/* Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. */}
+      <path
+        fill="#FFC107"
+        d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"
+      />
+    </svg>
   );
 }
